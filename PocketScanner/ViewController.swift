28ead7ac72b1b477eee2f0b298fd46c1c1b7f5ec
melvnl <melvinlim2000@gmail.com>
2022-04-25 17:32:43 +0700

    Initial Commit

---
+++ PocketScanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PocketScanner
+//
+//  Created by melvin on 25/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
